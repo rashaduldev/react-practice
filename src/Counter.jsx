ceac7c9fa395c44c51129375bdcef8b26db5807e
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-09 14:11:31 +0600

    counter Done

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -7,10 +7,18 @@ export default function Counter() {
         const newCount =count+1;
         setCount (newCount);
     }
+    const reduceHandle=()=>{
+        const newCount =count-1;
+        setCount (newCount);
+    }
+
     return(
         <div style={{border:'2px solid red',padding:'25px', borderRadius:'1rem'}}>
             <h2>Counter:{count}</h2>
-            <button style={{border:'1px solid black',backgroundColor:'gray',color:'white'}} onClick={clickHandle}>Click</button>
+         <div style={{gap:'15px',display:'flex',justifyContent:'center'}}>
+         <button style={{border:'1px solid black',backgroundColor:'gray',color:'white'}} onClick={clickHandle}>Click</button>
+            <button style={{border:'1px solid black',backgroundColor:'gray',color:'white'}} onClick={reduceHandle}>Reduce</button>
+         </div>
         </div>
     )
 }
