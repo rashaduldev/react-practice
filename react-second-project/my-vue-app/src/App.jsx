aba5369871288f482cb146d11715d38883ebb30f
Rashadul Islam <rashadul.dev@gmail.com>
2023-09-09 21:48:55 +0600

    added

--- react-second-project/my-vue-app/src/App.jsx
+++ react-second-project/my-vue-app/src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 import Users from './Users'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- react-second-project/my-vue-app/src/Users.jsx
+++ react-second-project/my-vue-app/src/Users.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 
 export default function Users() {
-    const [user,setUser]=useState;
+    const [users, setUsers]=useState([]);
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(res=>res.json())
