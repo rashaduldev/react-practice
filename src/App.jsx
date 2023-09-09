ddf14596a3f6e21539c6e14b8a3e5cda66f781d4
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-09 10:48:40 +0600

    code added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Person from './Person'
 import Team from './Team'
+import BookStore from './BookStore'
 
 function App() {
 const persons=['Sakib','jasim','Rubel','Shoriful'];
@@ -14,11 +15,21 @@ const practiceTeam=[
   {id:4,name:'Sakib',age:23},
   {id:5,name:'Rahul',age:28},
   {id:6,name:'Rubel',age:20}
-]
+];
+
+const books=[
+  {id:1,name:'React',price:230},
+  {id:2,name:'Java',price:230},
+  {id:3,name:'Javascript',price:230},
+  {id:4,name:'PHP',price:230},
+  {id:5,name:'Python',price:230},
+  {id:6,name:'Laravel',price:230}
+];
 
   return (
     <>
     <h1>Vite + React</h1>
+    <BookStore books={books}></BookStore>
     {
       practiceTeam.map(teammeet=><Team person={teammeet}></Team>)
     }
---
+++ src/books.jsx
@@ -0,0 +1,8 @@
+export default function Book(book) {
+    return (
+        <div>
+            <h3>Book Name:</h3>
+            <h4>Book Prices:</h4>
+        </div>
+    )
+}
