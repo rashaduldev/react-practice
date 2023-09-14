790f12ce034e306d9ae52260e973bb36b395cd84
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-14 10:22:03 +0600

    Fake Data Added

--- react-second-project/New folder/Cafe-Project/src/components/Header.jsx
+++ react-second-project/New folder/Cafe-Project/src/components/Header.jsx
@@ -1,9 +1,11 @@
+import Blogs from "./Blogs/Blogs";
 
 
 const Header = () => {
     return (
         <div>
                   <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+                  <Blogs></Blogs>
         </div>
     );
 };
