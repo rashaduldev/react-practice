edee6708e3a46b6d9c9325d19a11de21dac2b9e4
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-14 16:19:49 +0600

    Button working good

--- react-second-project/New folder/Cafe-Project/src/App.jsx
+++ react-second-project/New folder/Cafe-Project/src/App.jsx
@@ -2,15 +2,19 @@ import './App.css'
 import Header from './components/Header'
 import Blogs from './components/Blogs/Blogs'
 import Bookmarks from './components/Bookmarks/Bookmarks'
+// https://kit.fontawesome.com/be2168ea2b.js
 
 function App() {
+  const bookMarkHandle=()=>{
+    console.log("clicked");
+  }
 
   return (
     <>
    
     <Header></Header>
-     <div className='md:flex mx-6'>
-        <Blogs></Blogs>
+     <div className='md:flex  max-w-7xl mx-auto'>
+        <Blogs bookMarkHandle={bookMarkHandle}></Blogs>
         <Bookmarks></Bookmarks>
      </div>
     
--- react-second-project/New folder/Cafe-Project/src/components/Blogs/Blogs.jsx
+++ react-second-project/New folder/Cafe-Project/src/components/Blogs/Blogs.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Blog from "../Blog/Blog";
 
-const Blogs = () => {
+const Blogs = ({bookMarkHandle}) => {
     const [blogs,setBlogs]=useState([]);
     useEffect(()=>{
         fetch('blog.json')
@@ -18,7 +18,10 @@ const Blogs = () => {
           {
                 blogs.map(blog=><Blog 
                     key={blog.id} 
-                    blog={blog}>
+                    blog={blog}
+                    bookMarkHandle={bookMarkHandle}
+                    
+                    >
                         
                     </Blog>)
             }
