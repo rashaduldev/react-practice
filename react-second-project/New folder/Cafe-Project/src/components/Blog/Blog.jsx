bd22bb1ac19b2652927f3507f05907791e9bec8f
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-14 10:51:38 +0600

    Update Blog.jsx

--- react-second-project/New folder/Cafe-Project/src/components/Blog/Blog.jsx
+++ react-second-project/New folder/Cafe-Project/src/components/Blog/Blog.jsx
@@ -1,10 +1,11 @@
 
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
+    const {title}=blog;
     console.log(blog);
     return (
         <div>
-            
+            <h2 className='text-4xl'>{title}</h2>
         </div>
     );
 };
