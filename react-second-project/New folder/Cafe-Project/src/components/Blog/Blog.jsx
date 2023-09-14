4978b416527c46a0ff382b70c00ba8ff0642c806
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-14 16:20:56 +0600

    Button Handle Done

--- react-second-project/New folder/Cafe-Project/src/components/Blog/Blog.jsx
+++ react-second-project/New folder/Cafe-Project/src/components/Blog/Blog.jsx
@@ -1,27 +1,35 @@
 
+import { FaBeer } from 'react-icons/fa';
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
+const Blog = ({blog ,bookMarkHandle}) => {
     const {title,cover,author,author_img,posted_date,reading_time,hashtags,id}=blog;
     console.log(blog);
     return (
         <div className='my-10'>
-            <img className='w-full' src={cover} alt={`cover picture of the ${title}`} />
-            <div className='flex justify-between my-5'>
+            <img className='w-full rounded' src={cover} alt={`cover picture of the ${title}`} />
+            <div className='flex justify-between items-center'>
                 <div className='flex gap-3 items-center'>
                 <img className='w-14' src={author_img} alt="" />
-                <div>
+                <div className='my-5'>
                     <h3 className='text-2xl'>{author}</h3> 
                       <h2>{posted_date} <span className='font-bold'>(4 days ago)</span></h2>  
                 </div>
                 </div>
-                <div>
+                <div className='my-5 flex items-center gap-4'>
                     <span>{reading_time} min read</span>
+                    <button onClick={bookMarkHandle} className='text-2xl'><FaBeer /></button>
                 </div>
             </div>
-            <h2 className='text-4xl'>{title}</h2>
+            <h2 className='text-4xl my-5'>{title}</h2>
+            <div>
+                <p>
+                    {
+                        hashtags.map((hash,idx)=><span key={hash}><a href="">{hash}</a></span>)
+                    }
+                </p>
+            </div>
             <hr />
         </div>
-        
     );
 };
 Blog.prototype={
--- react-second-project/New folder/Cafe-Project/src/components/Header.jsx
+++ react-second-project/New folder/Cafe-Project/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import profile from '../../dist/assets/images/profile.png'
+// import Blogs from "./Blogs/Blogs";
 
 
 const Header = () => {
     return (
         <div>
-                  <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
+                  <header className='flex justify-between items-center py-4  max-w-7xl mx-auto border-b-2'>
                   <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
                   <img src={profile} alt="" />
                   </header>
