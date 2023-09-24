f84ef38e649c841e5ae21f1dd65b6de5646f2b87
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-24 16:41:43 +0600

    Update Simpleform.jsx

--- react-second-project/Form-Master/src/components/Simpleform/Simpleform.jsx
+++ react-second-project/Form-Master/src/components/Simpleform/Simpleform.jsx
@@ -3,11 +3,17 @@ const Simpleform = () => {
     const Handlesubmit=e=>{
         e.preventDefault();
         console.log('Submitted');
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+
     }
     return (
         <div>
             <form action="" onSubmit={Handlesubmit}>
-                <input type="text"  placeholder='Input'/> 
+                <input type="text" name="name" placeholder='Name'/> <br />
+                <input type="email" name="email" placeholder='Email'/> <br />
+                <input type="phone" name="phone" placeholder='phone'/> <br />
                 <input type="submit" value="submit" />
             </form>
         </div>
